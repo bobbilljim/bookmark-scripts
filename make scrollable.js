2874729ae57daceba7feeeddfe64633aa5d388dd
bobbilljim <bob.bill.jim@gmail.com>
2021-02-23 23:23:52 +1300

    Now checking overflow and overflow y and scanning entire document for computed styles instead of declared.

--- make scrollable.js
+++ make scrollable.js
@@ -1,11 +1,17 @@
-javascript: ! function () {
-    var l = document.querySelectorAll("*[style]");
-    Array.prototype.forEach.call(l, function (l) {
-        "hidden" === (l.style["overflow-y"] || "") && (l.style["overflow-y"] = "scroll")
-    })
-}();
-
-
+javascript: ! function() {
+        var t, o;
+        var e = document.querySelectorAll("body *");
+        for (t = 0; t < e.length; t++) {
+            o = getComputedStyle(e[t]);
+            if (o["overflow"] === "hidden") {
+                e[t].style["overflow"] = "scroll";
+            }
+            if (o["overflow-y"] === "hidden") {
+                e[t].style["overflow-y"] = "scroll";
+            }
+        }
+    }();
 
 //uglified:
-javascript:!function(){var l=document.querySelectorAll("*[style]");Array.prototype.forEach.call(l,function(l){"hidden"===(l.style["overflow-y"]||"")&&(l.style["overflow-y"]="scroll")})}();
+javascript:!function(){var t,o;var e=document.querySelectorAll("body *");for(t=0;t<e.length;t++){o=getComputedStyle(e[t]);if(o.overflow==="hidden"){e[t].style.overflow="scroll"}
+if(o["overflow-y"]==="hidden"){e[t].style["overflow-y"]="scroll"}}}()
