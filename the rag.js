52f2ceebcb234bf53af5747fc5b401e27f19cb7b
bobbilljim <bob.bill.jim@gmail.com>
2021-08-02 14:26:21 +1200

    Create the rag.js

---
+++ the rag.js
@@ -0,0 +1,7 @@
+javascript: (function () {
+    var maxId = setTimeout(function(){}, 0);
+    for(var i=0; i < maxId; i+=1) { 
+        clearTimeout(i);
+    }
+    document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(e => {e.style["display"] = "block"; e.style["opacity"] = "100%"; e.style["color"] = "black"});
+})()
