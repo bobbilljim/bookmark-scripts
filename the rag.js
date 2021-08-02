d242039f43da4b4e58d328207bc801bec76a70bd
bobbilljim <bob.bill.jim@gmail.com>
2021-08-02 14:48:32 +1200

    Update the rag.js

--- the rag.js
+++ the rag.js
@@ -3,8 +3,15 @@ javascript: (function () {
     for(var i=0; i < maxId; i+=1) { 
         clearTimeout(i);
     }
+    document.getElementsByClassName('header__sub-btn')[0].style['display'] = 'none'
+    document.getElementsByClassName('header__sign-in-btn')[0].style['display'] = 'none'
+    document.querySelectorAll('span[class="story-card__label"]').forEach(e => e.innerText = 'Free 😉');
+    document.getElementById('premium-toaster').style["display"] = "none"
+    document.querySelectorAll('section[class="article-offer"]').forEach(e => e.style["display"] = "none");
     document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(e => {e.style["display"] = "block"; e.style["opacity"] = "100%"; e.style["color"] = "black"});
 })()
 
 
-javascript:(function(){for(var maxId=setTimeout(function(){},0),i=0;i<maxId;i+=1)clearTimeout(i);document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(l=>{l.style.display="block",l.style.opacity="100%",l.style.color="black"});})()
+javascript:(function(){<minifed goes here>})()
+
+javascript:(function(){for(var maxId=setTimeout(function(){},0),i=0;i<maxId;i+=1)clearTimeout(i);document.getElementsByClassName("header__sub-btn")[0].style.display="none",document.getElementsByClassName("header__sign-in-btn")[0].style.display="none",document.querySelectorAll('span[class="story-card__label"]').forEach(e=>e.innerText="Free 😉"),document.getElementById("premium-toaster").style.display="none",document.querySelectorAll('section[class="article-offer"]').forEach(e=>e.style.display="none"),document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(e=>{e.style.display="block",e.style.opacity="100%",e.style.color="black"});})()
