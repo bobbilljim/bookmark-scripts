c8bfd7f917916cd4898c72c76e9a438244804d90
bobbilljim <bob.bill.jim@gmail.com>
2021-08-02 14:28:24 +1200

    Update the rag.js

--- the rag.js
+++ the rag.js
@@ -5,3 +5,6 @@ javascript: (function () {
     }
     document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(e => {e.style["display"] = "block"; e.style["opacity"] = "100%"; e.style["color"] = "black"});
 })()
+
+
+javascript:(function(){for(var maxId=setTimeout(function(){},0),i=0;i<maxId;i+=1)clearTimeout(i);document.querySelectorAll('p[class="nLbYqwiwtKuXHhsvGA"]').forEach(l=>{l.style.display="block",l.style.opacity="100%",l.style.color="black"});})()
