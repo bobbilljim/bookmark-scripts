766cca94aed7bf3cf7304c8e5d00d19a2b7d0edb
bobbilljim <bob.bill.jim@gmail.com>
2021-10-07 13:59:03 +1300

    Update the rag.js

--- the rag.js
+++ the rag.js
@@ -1,3 +1,43 @@
+//UserScript
+var hide = (elem) => {
+    if(elem){
+        elem.style.display = 'none';
+    }
+}
+
+hide(document.getElementsByClassName('action-bar')[0])
+
+var articleHeader = document.getElementsByClassName('article__header-premium')[0];
+if(articleHeader){
+    articleHeader.classList.remove('article__header-premium');
+};
+
+window.addEventListener('load', function() {
+    'use strict';
+    var maxId = setTimeout(function(){}, 0);
+    for(var i=0; i < maxId; i+=1) {
+        clearTimeout(i);
+    }
+    var theClassName = Object.entries(Array.from(document.querySelectorAll('p')).reduce((acc, current) => {
+        if (acc[current.className] === undefined){
+            acc[current.className] = 1;
+        } else {
+            acc[current.className]++;
+        }
+        return acc;
+    }, {})).reduce((max, current) => {
+        return max[1] > current[1] ? max : current
+    })[0];
+
+    hide(document.getElementsByClassName('header__sub-btn')[0])
+    hide(document.getElementsByClassName('header__sign-in-btn')[0])
+    document.querySelectorAll('span[class="story-card__label"]').forEach(e => {e.innerText = 'Free 😉'; return null;});
+    hide(document.getElementById('premium-toaster'))
+    document.querySelectorAll('section[class="article-offer"]').forEach(e => {e.style.display = 'none'; return null});
+    document.querySelectorAll('p.' + theClassName + ',span.' + theClassName).forEach(e => {e.style.display = "block"; e.style.opacity = "100%"; e.style.color = "black"});
+})();
+
+//bookmarklet
 javascript: (function () {
     var hide = (elem) => {
         if(elem){
